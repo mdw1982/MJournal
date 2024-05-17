53af5f1d1cd1923602d97324e6173733e002a3a5
mweaver <mdw1982@gmail.com>
2024-05-17 17:33:53 -0400

    updated 5.17.24 1533

--- dbmoves.py
+++ dbmoves.py
@@ -2,22 +2,30 @@ import os.path
 import shutil as sh
 from os.path import exists
 import datetime as dt
+import FreeSimpleGUI as sg
+from settings import detect_os
 
 def detach(dbname):
-    src = os.path.relpath(dbname)
-    dest = os.path.relpath('olddb')
-    epoch = dt.datetime.now().strftime('%s')
+    try:
+        src = os.path.join(os.getcwd(),  dbname)
+        dest = os.path.relpath('olddb')
+        if detect_os() == 'windows':
+            epoch = dt.datetime.now().strftime('%S')
+        else:
+            epoch = dt.datetime.now().strftime('%s')
 
-    print(src)
-    print(dest)
-    if exists(os.path.join(dest, dbname)):
-        temp,ext = dbname.split('.')
-        ndbname = temp+'_'+epoch+'.db'
-        os.rename(dbname,ndbname)
-        src = os.path.relpath(ndbname)
-        sh.move(src, dest)
-    else:
-        sh.move(src,dest)
+        print(src)
+        print(dest)
+        if exists(os.path.join(dest, dbname)):
+            temp,ext = dbname.split('.')
+            ndbname = temp+'_'+epoch+'.db'
+            os.rename(dbname,ndbname)
+            src = os.path.relpath(ndbname)
+            sh.move(src, dest)
+        else:
+            sh.move(src,dest)
+    except Exception as e:
+        sg.PopupError(f"Something went wrong detaching the database: {dbname}\n{e}")
 
 def attach(dbname):
     srcfldr = os.path.relpath('olddb')
