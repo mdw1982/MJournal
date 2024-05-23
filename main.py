0d40ddec7e13b7fdb66342c1770ee6107b4e43a2
mweaver <mdw1982@gmail.com>
2024-05-23 15:16:13 -0400

    updated 5.23.24 1515

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ platform = detect_os()
 
 __version__ = defaults['version']
 
-mainWindowSize = (1090, 790)
+mainWindowSize = (1090, 820)
 new_ent_win = (650, 610)    # new entry screen/window size
 win_location = (360, 90)
 searchWindowSize = (990, 630)
@@ -1483,7 +1483,7 @@ def main():
     ]
     right_click_menu = ['', ['Copy', 'Paste', 'Select All']]
     col2 = [
-        [sg.Input('',key='E_TITLE', size=(40, 1), font=std_font, pad=(5, 5), readonly=True, visible=False)],
+        [sg.Input('',key='E_TITLE', size=(40, 1), font=std_font, pad=(5, 5), readonly=False, visible=True)],
         [sg.Multiline('', font=std_font, size=(89, 23), pad=(5, 5), key='VIEW',
                       right_click_menu=right_click_menu, autoscroll=True, disabled=True)]
     ]
@@ -1502,7 +1502,7 @@ def main():
          sg.Button('Change Database', key='DBCHANGE')]
     ]
     func_frame = [
-        [sg.Push(), sg.Button('Reload Tree', key='ReloadTree'),sg.Button('Reload Program', key='Reload', tooltip=tp_reload(), visible=True),
+        [sg.Push(), sg.Button('Reload Tree', key='ReloadTree'),sg.Button('Reload Program', key='Reload', tooltip=tp_reload(), visible=False),
          sg.Button('Update Entry (F5)', key='UpdateEntry'), sg.Button('New Entry (F8)', key='New Entry Window'),
          sg.Button('Exit (F12)', key='quit')]
     ]
@@ -1666,6 +1666,7 @@ def main():
                     dbo = DBConn(values['DBNAME'])
                     print(f"New dbo database: {dbo.database}")
                     window['_TREE_'].update(load_tree_data())
+                    window['E_TITLE'].update('')
                     window['VIEW'].update('')
                     window['sbar'].update(
                         f"Date: {dt.datetime.now().strftime('%Y-%m-%d')}\t Connected to Database: {dbo.database}:: \tCurrent Theme: {curr_theme}")
@@ -1674,25 +1675,14 @@ def main():
                                auto_close=True, auto_close_duration=3)
                 except Exception as e:
                     dbo.close()
-                    fsize = os.path.getsize(os.path.join(os.getcwd(),values['DBNAME']))
-                    if fsize == 0:
-                        sg.PopupError(f"ERROR_[DBC1] The requested database file {values['DBNAME']} has {fsize} bytes. Most likely\n"
-                                      f"this database file has been damaged in some way. Please restore from backups...\n"
-                                      f"Returning to the previous database...")
-                        dbmoves.damaged_db(values['DBNAME'])
-                    else:
-                        sg.PopupError(f"ERROR_[DBC2] I have experienced an error switching database to {values['DBNAME']}: {e}\n"
+                    sg.PopupError(f"ERROR_[DBC2] I have experienced an error switching database to {values['DBNAME']}: {e}\n"
                                   f"I'm returning you to the previous database until this problem can be corrected...")
-                        dbmoves.damaged_db(values['DBNAME'])
-                finally:
-                    set_new_db(prevdb)
                     dbo = DBConn(prevdb)
-                    print(f"going back to database: {dbo.database}")
                     window['_TREE_'].update(load_tree_data())
+                    window['E_TITLE'].update('')
                     window['VIEW'].update('')
                     window['sbar'].update(
                         f"Date: {dt.datetime.now().strftime('%Y-%m-%d')}\t Connected to Database: {dbo.database}:: \tCurrent Theme: {curr_theme}")
-                    window['DBNAME'].update(values=reload_dblist(), size=(30, 10))
                     window.refresh()
             case 'UpdateEntry':
                 # currid = values['_TREE_'][0]
