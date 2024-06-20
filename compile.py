8f6ba0e429833671d41be76aa1a9378b41c315be
mweaver <mdw1982@gmail.com>
2024-06-20 12:46:21 -0400

    updated 6.20.24 1246

--- compile.py
+++ compile.py
@@ -26,6 +26,9 @@ if detect_os() == 'Linux':
 if detect_os() == 'windows':
     #os.system('pyinstaller --noconfirm --onefile --windowed -n MJournal.exe main.py')
     os.system('pyinstaller --noconfirm --onefile --windowed --icon "C:/Users/mweaver.MDW1982/PycharmProjects/MJournal/images/MjournalIcon_36x36.ico" --name "MJournal.exe" --version-file "C:/Users/mweaver.MDW1982/PycharmProjects/MJournal/vfileMJournal.txt"  "C:/Users/mweaver.MDW1982/PycharmProjects/MJournal/main.py"')
-    os.system('pyinstaller --noconfirm --onefile --windowed -n dbbackup.exe dbbackup.py')
-    os.system('pyinstaller --noconfirm --onefile --windowed -n setup.exe setup.py')
-    os.system('pyinstaller --noconfirm --onefile --windowed -n Unlock Unlock.py')
+    #os.system('pyinstaller --noconfirm --onefile --windowed -n dbbackup.exe dbbackup.py')
+    os.system('pyinstaller --noconfirm --onefile --windowed --icon "C:/Users/mweaver.MDW1982/PycharmProjects/MJournal/images/MjournalIcon_36x36.ico" --name "dbbackup.exe" --version-file "C:/Users/mweaver.MDW1982/PycharmProjects/MJournal/vfileDBbu.txt"  "C:/Users/mweaver.MDW1982/PycharmProjects/MJournal/dbbackup.py"')
+    #os.system('pyinstaller --noconfirm --onefile --windowed -n setup.exe setup.py')
+    os.system('pyinstaller --noconfirm --onefile --windowed --icon "C:/Users/mweaver.MDW1982/PycharmProjects/MJournal/images/MjournalIcon_36x36.ico" --name "setup.exe" --version-file "C:/Users/mweaver.MDW1982/PycharmProjects/MJournal/vfileSetup.txt"  "C:/Users/mweaver.MDW1982/PycharmProjects/MJournal/setup.py"')
+    #os.system('pyinstaller --noconfirm --onefile --windowed -n Unlock Unlock.py')
+    os.system('pyinstaller --noconfirm --onefile --windowed --icon "C:/Users/mweaver.MDW1982/PycharmProjects/MJournal/images/MjournalIcon_36x36.ico" --name "Unlock.exe" --version-file "C:/Users/mweaver.MDW1982/PycharmProjects/MJournal/vfileUnlock.txt"  "C:/Users/mweaver.MDW1982/PycharmProjects/MJournal/Unlock.py"')
